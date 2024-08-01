8b6d4b963b5c50024286c68f0e182d702af73ef0
Tom (plebeius.eth) <tom@plebbit.com>
2024-08-01 16:32:16 +0200

    follow tg docs for escaping characters

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -4,26 +4,15 @@ import sanitizeHtml from 'sanitize-html';
 const md = new MarkdownIt();
 
 function escapeMarkdownV2(text: string): string {
-    return text
-        .replace(/\\/g, '\\\\')  // escape the backslash first
-        .replace(/_/g, '\\_')
-        .replace(/\*/g, '\\*')
-        .replace(/\[/g, '\\[')
-        .replace(/\]/g, '\\]')
-        .replace(/\(/g, '\\(')
-        .replace(/\)/g, '\\)')
-        .replace(/~/g, '\\~')
-        .replace(/`/g, '\\`')
-        .replace(/>/g, '\\>')
-        .replace(/#/g, '\\#')
-        .replace(/\+/g, '\\+')
-        .replace(/-/g, '\\-')
-        .replace(/=/g, '\\=')
-        .replace(/\|/g, '\\|')
-        .replace(/{/g, '\\{')
-        .replace(/}/g, '\\}')
-        .replace(/\./g, '\\.')
-        .replace(/!/g, '\\!');
+    const escapeChars = [
+        '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'
+    ];
+    let escapedText = text;
+    escapeChars.forEach(char => {
+        const escapedChar = '\\' + char;
+        escapedText = escapedText.split(char).join(escapedChar);
+    });
+    return escapedText;
 }
 
 function sanitizeMarkdown(content: string): string {
