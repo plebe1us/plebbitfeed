64425de6d421cc0b39768cd57e21dab83b4519f4
Tom (plebeius.eth) <tom@plebbit.com>
2025-05-23 15:26:16 +0200

    add prettier, remove unnecessary packages, move file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { startPlebbitFeedBot } from "./plebbitfeed-chat/plebbitfeed-chat-bot.js";
+import { startPlebbitFeedBot } from "./plebbitfeed-chat-bot.js";
 import { Scenes, Telegraf } from "telegraf";
 import { Logger } from "tslog";
 import Plebbit from "@plebbit/plebbit-js";
@@ -9,61 +9,51 @@ export const log = new Logger();
 dotenv.config();
 
 if (!process.env.BOT_TOKEN) {
-    throw new Error("BOT_TOKEN is not set");
+  throw new Error("BOT_TOKEN is not set");
 }
 export const plebbitFeedTgBot = new Telegraf<Scenes.WizardContext>(
-    process.env.BOT_TOKEN!,
-    {
-        telegram: {
-            agent: new Agent({ keepAlive: false }),
-        },
-    }
+  process.env.BOT_TOKEN!,
+  {
+    telegram: {
+      agent: new Agent({ keepAlive: false }),
+    },
+  },
 );
 
 export const plebbit = await Plebbit({
   kuboRpcClientsOptions: [`http://localhost:50019/api/v0`],
   chainProviders: {
     eth: {
-      urls: [
-        "ethers.js",
-        "https://ethrpc.xyz",
-        "viem"
-      ],
-      chainId: 1
+      urls: ["ethers.js", "https://ethrpc.xyz", "viem"],
+      chainId: 1,
     },
     avax: {
-      urls: [
-        "https://api.avax.network/ext/bc/C/rpc"
-      ],
-      chainId: 43114
+      urls: ["https://api.avax.network/ext/bc/C/rpc"],
+      chainId: 43114,
     },
     matic: {
-      urls: [
-        "https://polygon-rpc.com"
-      ],
-      chainId: 137
-    }
-  }
+      urls: ["https://polygon-rpc.com"],
+      chainId: 137,
+    },
+  },
 });
 plebbit.on("error", (error: any) => {
-    log.error(error.details);
+  log.error(error.details);
 });
 
 const start = async () => {
-    try {
-        plebbitFeedTgBot.launch();
-        // Started message
-        if (plebbitFeedTgBot)
-            plebbitFeedTgBot.telegram
-                .getMe()
-                .then((res) =>
-                    console.log(`Bot started on https://t.me/${res.username}`)
-                );
-        await Promise.all([
-            startPlebbitFeedBot(plebbitFeedTgBot),
-        ]);
-    } catch (error) {
-        log.error(error);
-    }
+  try {
+    plebbitFeedTgBot.launch();
+    // Started message
+    if (plebbitFeedTgBot)
+      plebbitFeedTgBot.telegram
+        .getMe()
+        .then((res) =>
+          console.log(`Bot started on https://t.me/${res.username}`),
+        );
+    await Promise.all([startPlebbitFeedBot(plebbitFeedTgBot)]);
+  } catch (error) {
+    log.error(error);
+  }
 };
 start();
---
+++ src/plebbitfeed-chat-bot.ts
@@ -0,0 +1,290 @@
+import * as fs from "fs";
+import { Scenes, Telegraf } from "telegraf";
+import { log, plebbit } from "./index.js";
+import fetch from "node-fetch";
+import PQueue from "p-queue";
+
+const queue = new PQueue({ concurrency: 1 });
+const historyCidsFile = "history.json";
+let processedCids: Set<string> = new Set();
+
+async function scrollPosts(
+  address: string,
+  tgBotInstance: Telegraf<Scenes.WizardContext>,
+  plebbit: typeof import("./index.js").plebbit,
+  subInstance: any,
+) {
+  log.info("Checking sub: ", address);
+  try {
+    log.info("Sub loaded");
+    let currentPostCid = subInstance.lastPostCid;
+    let counter = 0;
+    while (currentPostCid && counter < 20) {
+      counter += 1;
+      log.info(`Processing CID: ${currentPostCid}`);
+      if (currentPostCid && !processedCids.has(currentPostCid)) {
+        const newPost = await plebbit.getComment(currentPostCid);
+        const postData = {
+          title: newPost.title ? newPost.title : "",
+          content: newPost.content ? newPost.content : "",
+          postCid: newPost.postCid,
+          link: newPost.link,
+          cid: newPost.cid,
+          subplebbitAddress: newPost.subplebbitAddress,
+          timestamp: newPost.timestamp,
+          removed: newPost.removed ? newPost.removed : false,
+          deleted: newPost.deleted ? newPost.deleted : false,
+        };
+        postData.title = postData.title
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;");
+
+        postData.content = postData.content
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;");
+
+        // Check if the post is older than 24 hours
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (currentTime - postData.timestamp > 24 * 60 * 60) {
+          log.info("Post is older than 24 hours, skipping.");
+          currentPostCid = newPost.previousCid;
+          continue;
+        }
+
+        // Check if the post is removed or deleted
+        if (postData.removed || postData.deleted) {
+          log.info("Post is removed or deleted, skipping.");
+          currentPostCid = newPost.previousCid;
+          continue;
+        }
+
+        if (postData.title.length + postData.content.length > 900) {
+          if (postData.title.length > 900) {
+            const truncated = postData.title.substring(0, 900);
+            postData.title =
+              truncated.substring(0, truncated.length - 3) + "...";
+            postData.content = postData.content.substring(0, 900) + "...";
+          } else {
+            const truncated = postData.content.substring(
+              0,
+              900 - postData.title.length,
+            );
+            postData.content =
+              truncated.substring(0, truncated.length - 3) + "...";
+          }
+        }
+        const captionMessage = `<b>${postData.title ? postData.title + " " : ""}</b>${newPost.spoiler ? "[SPOILER]" : newPost.nsfw ? "[NSFW]" : ""}\n${postData.content}\n\nSubmitted on <a href="https://plebchan.eth.limo/#/p/${newPost.subplebbitAddress}">p/${newPost.subplebbitAddress}</a> by u/${newPost.author.address.includes(".") ? newPost.author.address : newPost.author.shortAddress}`;
+
+        if (postData.link) {
+          await queue.add(async () => {
+            tgBotInstance.telegram
+              .sendPhoto(process.env.FEED_BOT_CHAT!, postData.link!, {
+                parse_mode: "HTML",
+                caption: captionMessage,
+                has_spoiler: newPost.spoiler || newPost.nsfw,
+                reply_markup: {
+                  inline_keyboard: [
+                    [
+                      {
+                        text: "View on Seedit",
+                        url: `https://seedit.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                      },
+                      {
+                        text: "View on Plebchan",
+                        url: `https://plebchan.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                      },
+                    ],
+                  ],
+                },
+              })
+              .then(() => {
+                if (currentPostCid) {
+                  processedCids.add(currentPostCid);
+                }
+              })
+              .catch((error: any) => {
+                log.error(error);
+                // if the link is not a valid image, send the caption
+                tgBotInstance.telegram
+                  .sendMessage(process.env.FEED_BOT_CHAT!, captionMessage, {
+                    parse_mode: "HTML",
+                    reply_markup: {
+                      inline_keyboard: [
+                        [
+                          {
+                            text: "View on Seedit",
+                            url: `https://seedit.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                          },
+                          {
+                            text: "View on Plebchan",
+                            url: `https://plebchan.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                          },
+                        ],
+                      ],
+                    },
+                  })
+                  .then(() => {
+                    if (currentPostCid) {
+                      processedCids.add(currentPostCid);
+                    }
+                  })
+                  .catch((fallbackError: any) => {
+                    log.error(fallbackError);
+                  });
+              });
+
+            await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
+          });
+        } else {
+          await queue.add(async () => {
+            tgBotInstance.telegram
+              .sendMessage(process.env.FEED_BOT_CHAT!, captionMessage, {
+                parse_mode: "HTML",
+                reply_markup: {
+                  inline_keyboard: [
+                    [
+                      {
+                        text: "View on Seedit",
+                        url: `https://seedit.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                      },
+                      {
+                        text: "View on Plebchan",
+                        url: `https://plebchan.eth.limo/#/p/${postData.subplebbitAddress}/c/${postData.cid}`,
+                      },
+                    ],
+                  ],
+                },
+              })
+              .then(() => {
+                if (currentPostCid) {
+                  processedCids.add(currentPostCid);
+                }
+              })
+              .catch((error: any) => {
+                log.error(error);
+              });
+            await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
+          });
+        }
+        log.info("New post: ", postData);
+        currentPostCid = newPost.previousCid;
+      } else {
+        //log.info("Already processsed: ", currentPostCid);
+        const post = await plebbit.getComment(currentPostCid);
+        currentPostCid = post.previousCid;
+      }
+    }
+  } catch (e) {
+    log.error(e);
+  }
+  log.info("Finished on ", address);
+}
+
+function loadOldPosts() {
+  try {
+    const data = fs.readFileSync(historyCidsFile, "utf8");
+    const parsedData = JSON.parse(data);
+    processedCids = new Set(parsedData.Cids); // Ensure uniqueness
+  } catch (error) {
+    log.error(error);
+    throw new Error();
+  }
+}
+
+function savePosts() {
+  try {
+    const dataToSave = { Cids: Array.from(processedCids) };
+    fs.writeFileSync(
+      historyCidsFile,
+      JSON.stringify(dataToSave, null, 2),
+      "utf8",
+    );
+  } catch (error) {
+    log.error("Error saving json file");
+  }
+}
+
+export async function startPlebbitFeedBot(
+  tgBotInstance: Telegraf<Scenes.WizardContext>,
+) {
+  log.info("Starting plebbit feed bot");
+
+  if (!process.env.FEED_BOT_CHAT || !process.env.FEED_BOT_CHAT) {
+    throw new Error("FEED_BOT_CHAT or BOT_TOKEN not set");
+  }
+  while (true) {
+    loadOldPosts();
+    console.log("Length of loaded posts: ", processedCids.size);
+    const subs = await fetchSubs();
+    await Promise.all(
+      subs.map(async (subAddress: string) => {
+        try {
+          log.info("Loading sub ", subAddress);
+          const startTime = performance.now();
+          const subInstance: any = await Promise.race([
+            plebbit.getSubplebbit(subAddress),
+            new Promise((_, reject) => {
+              setTimeout(
+                () => {
+                  reject(new Error("Operation timed out after 5 minutes"));
+                },
+                5 * 60 * 1000,
+              );
+            }),
+          ]);
+          const endTime = performance.now();
+          log.info("Time to load sub: ", endTime - startTime);
+          if (subInstance.address) {
+            await Promise.race([
+              scrollPosts(
+                subInstance.address,
+                tgBotInstance,
+                plebbit,
+                subInstance,
+              ),
+              new Promise((_, reject) => {
+                setTimeout(
+                  () => {
+                    reject(
+                      new Error(
+                        "Timedout after 6 minutes of post crawling on " +
+                          subInstance.address,
+                      ),
+                    );
+                  },
+                  6 * 60 * 1000,
+                );
+              }),
+            ]);
+          }
+        } catch (e) {
+          log.info(e);
+          log.info(subAddress);
+        }
+      }),
+    );
+    log.info("saving new posts");
+    savePosts();
+  }
+}
+
+export async function fetchSubs() {
+  let subs = [];
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json",
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch subs");
+    } else {
+      const data: any = await response.json();
+
+      subs = data.subplebbits.map((obj: any) => obj.address);
+    }
+  } catch (error) {
+    log.error("Error:", error);
+  }
+  return subs;
+}
